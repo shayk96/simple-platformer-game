704186ef53afba6cfbba11f2b0e166ad3d245b14
shayk <shay.kvasha@mail.huji.ac.il>
2023-06-28 17:54:41 +0300

    game works

--- Platform.py
+++ Platform.py
@@ -1,12 +1,19 @@
 import pygame
+import random
 
 HEIGHT = 450
 WIDTH = 400
 
 
 class Platform(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, base=False):
         super().__init__()
-        self.surf = pygame.Surface((WIDTH, 20))
-        self.surf.fill((255, 0, 0))
-        self.rect = self.surf.get_rect(center=(WIDTH / 2, HEIGHT - 10))
+        if base:
+            self.surf = pygame.Surface((WIDTH, 20))
+            self.surf.fill((255, 0, 0))
+            self.rect = self.surf.get_rect(center=(WIDTH / 2, HEIGHT - 10))
+        else:
+            self.surf = pygame.Surface((random.randint(50, 100), 12))
+            self.surf.fill((0, 255, 0))
+            self.rect = self.surf.get_rect(center=(random.randint(0, WIDTH - 10),
+                                                   random.randint(0, HEIGHT - 30)))
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from Player import Player
 from Platform import Platform
 import sys
+import random
 
 pygame.init()
 vec = pygame.math.Vector2  # 2 for two dimensional
@@ -13,19 +14,33 @@ FRIC = -0.12
 FPS = 60
 
 
+def plat_gen(platforms, all_sprites):
+    while len(platforms) < 7:
+        width = random.randrange(50, 100)
+        p = Platform()
+        p.rect.center = (random.randrange(0, WIDTH - width),
+                         random.randrange(-50, 0))
+        platforms.add(p)
+        all_sprites.add(p)
+
+
 def main():
     FramePerSec = pygame.time.Clock()
 
     pygame.display.set_caption("Game")
     displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
-    pt1 = Platform()
     p1 = Player(vec)
+    pt1 = Platform(base=True)
 
-    all_sprites = pygame.sprite.Group()
-    platforms = pygame.sprite.Group()
-    platforms.add(pt1)
+    all_sprites, platforms = pygame.sprite.Group(), pygame.sprite.Group()
     all_sprites.add(pt1)
+    platforms.add(pt1)
     all_sprites.add(p1)
+
+    for x in range(random.randint(5, 6)):
+        pl = Platform()
+        platforms.add(pl)
+        all_sprites.add(pl)
 
     while True:
         if pygame.key.get_pressed()[pygame.K_ESCAPE]:
@@ -46,11 +61,20 @@ def main():
         for entity in all_sprites:
             displaysurface.blit(entity.surf, entity.rect)
 
+        if p1.rect.top <= HEIGHT / 3:
+            p1.pos.y += abs(p1.vel.y)
+            for plat in platforms:
+                plat.rect.y += abs(p1.vel.y)
+                if plat.rect.top >= HEIGHT:
+                    plat.kill()
+        if p1.rect.top >= HEIGHT:
+            sys.exit()
+
+        plat_gen(platforms, all_sprites)
+
         pygame.display.update()
         FramePerSec.tick(FPS)
 
 
 if __name__ == '__main__':
     main()
-
-
